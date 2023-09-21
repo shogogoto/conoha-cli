ebf82d777c8d9acd682dd37bdb14a9e28837cb42
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-22 02:02:10 +0900

    fix lint

--- src/api/credentials.py
+++ src/api/credentials.py
@@ -3,11 +3,11 @@
 import requests
 
 from . import endpoints
-from . import environments as E
+from . import environments as e
 
 
 def issue_token_id() -> str:
     """ConoHa API用のトークンを発行する."""
     url = endpoints.Endpoints.IDENTITY.url("tokens")
-    res = requests.post(url, json=E.env_credentials(), timeout=3.0)
+    res = requests.post(url, json=e.env_credentials(), timeout=3.0)
     return res.json()["access"]["token"]["id"]
