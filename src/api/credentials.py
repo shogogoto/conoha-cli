8873492d1fd2ea59a6c69a7f4f0e4905902255f5
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-22 00:20:09 +0900

    fix lint

--- src/api/credentials.py
+++ src/api/credentials.py
@@ -1,3 +1,4 @@
+"""認証周りの処理."""
 import os
 
 import requests
@@ -5,7 +6,8 @@ import requests
 from . import endpoints
 
 
-def env_credentials():
+def env_credentials() -> dict:
+    """環境変数からトークン発行用認証情報を作成する."""
     try:
         return {
             "auth": {
@@ -16,31 +18,38 @@ def env_credentials():
                 "tenantId": os.environ["OS_TENANT_ID"],
             },
         }
-    except KeyError:
-        msg = "環境変数にConoHa VPSのAPI情報を設定してください:" \
-            + "OS_USERNAME: APIユーザー名, " \
-            + "OS_PASSWORD: APIユーザーのパスワード, " \
-            + "OS_TENANT_ID： テナント情報のテナントID"
-        raise KeyError(msg)
+    except KeyError as e:
+        msg = (
+            "環境変数にConoHa VPSのAPI情報を設定してください:"
+            "OS_USERNAME: APIユーザー名, "
+            "OS_PASSWORD: APIユーザーのパスワード, "
+            "OS_TENANT_ID: テナント情報のテナントID"
+        )
+        raise KeyError(msg) from e
 
 
-def env_region():
+def env_region() -> str:
+    """ConoHa VPSのリージョンを環境変数から取得する."""
     no_str = ""
     try:
         no_str = os.environ["OS_CONOHA_REGION_NO"]
-    except KeyError:
-        msg = "環境変数OS_CONOHA_REGION_NOに" + \
-              "ConoHa VPSのリージョン番号を指定してください"
-        raise KeyError(msg)
+    except KeyError as e:
+        msg = (
+            "環境変数OS_CONOHA_REGION_NOに"
+            "ConoHa VPSのリージョン番号を指定してください"
+        )
+        raise KeyError(msg) from e
 
     if not str.isdigit(no_str):
-        raise ValueError("OS_CONOHA_REGION_NO環境変数には数字を入力してください")
+        msg = "OS_CONOHA_REGION_NO環境変数には数字を入力してください"
+        raise ValueError(msg)
 
     return f"tyo{no_str}"
 
 
-def issue_token_id()->str:
+def issue_token_id() -> str:
+    """ConoHa API用のトークンを発行する."""
     url = "https://identity.tyo3.conoha.io/v2.0/tokens"
     url = endpoints.Endpoints.IDENTITY.url(env_region(), ["tokens"])
-    res = requests.post(url, json=env_credentials())
+    res = requests.post(url, json=env_credentials(), timeout=3.0)
     return res.json()["access"]["token"]["id"]
--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,9 +1,14 @@
-import os
+"""Conoha APIのサービス一覧."""
+from __future__ import annotations
+
 from enum import Enum
+from pathlib import Path
 
 
 # ref: https://www.conoha.jp/docs/?btn_id=docs--sidebar_docs
 class Endpoints(Enum):
+    """Conoha APIのエンドポイントとバージョン情報のペア."""
+
     ACCOUNT  = ("account",          "v1")
     COMPUTE  = ("compute",          "v2")
     VOLUME   = ("block.storage",    "v2")
@@ -15,11 +20,13 @@ class Endpoints(Enum):
     NETWORK  = ("networking",       "v2.0")
 
     def __init__(self, prefix:str, version:str) -> None:
+        """Enumのプロパティ設定."""
         self.prefix  = prefix
         self.version = version
 
     def url(self, region:str, suffixes:list[str])->str:
+        """エンドポイントを組み立てる."""
         p = self.prefix
         v = self.version
         base = f"https://{p}.{region}.conoha.io/{v}"
-        return os.path.join(base, *suffixes)
+        return Path.joinpath(base, *suffixes)
