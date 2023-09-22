497fe7717ec79ff6a9776bfdba72d2b91c03ff5b
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-22 15:49:20 +0900

    token付きheaders関数 作成

--- src/api/token.py
+++ src/api/token.py
@@ -1,4 +1,7 @@
 """認証周りの処理."""
+from __future__ import annotations
+
+from functools import cache
 
 import requests
 
@@ -6,8 +9,18 @@ from . import endpoints
 from .environments import env_credentials
 
 
+@cache
 def issue_token_id() -> str:
     """ConoHa API用のトークンを発行する."""
     url = endpoints.Endpoints.IDENTITY.url("tokens")
     res = requests.post(url, json=env_credentials(), timeout=3.0)
     return res.json()["access"]["token"]["id"]
+
+def token_headers() -> dict[str, str]:
+    """ConoHa API用のヘッダーを作成する."""
+    return {
+        "Accept": "application/json",
+        "X-Auth-Token": issue_token_id(),
+    }
+
+
