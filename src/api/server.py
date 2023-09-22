3846fa9b81e3a64f64919c6ac7e429d86a81b9ae
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-22 16:05:12 +0900

    契約中サーバー情報一覧を取得

---
+++ src/api/server.py
@@ -0,0 +1,82 @@
+"""契約中サーバー情報."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from enum import Enum, auto
+
+import requests
+from dateutil import parser
+from pytz import timezone
+
+from .endpoints import Endpoints
+from .environments import env_tenant_id
+from .token import token_headers
+
+
+class Status(Enum):
+    """契約中Serverの状態."""
+
+    ACTIVE  = auto()
+    SHUTOFF = auto()
+    REBOOT  = auto()
+
+    def is_shutoff(self) -> bool:
+        """シャットダウン済みか否か."""
+        return self == Status.SHUTOFF
+
+
+@dataclass(frozen=True)
+class Server:
+    r"""契約中のサーバー.
+
+    :param ipv4: 固定IPアドレス
+    :param created_at: 作成日時(JST)
+    :param updated_at: 更新日時(JST)
+    :param status: \[ACTIVE|SHUTOFF|REBOOT\]
+    :param image_id: イメージID
+    :param flavor_id: コア数などの設定ID
+    """
+
+    ipv4: str
+    created_at: datetime
+    updated_at: datetime
+    status: Status
+    image_id: str
+    flavor_id: str
+
+    def elapsed_from_created(self) -> timedelta:
+        """作成時からの経過時間を秒以下を省いて計算する."""
+        tz = timezone("Asia/Tokyo")
+        n = datetime.now(tz).replace(second=0, microsecond=0)
+        return n - self.created_at
+
+
+def utc2jst(utc_str: str) -> datetime:
+    """UTC文字列をJST Datetimeへ変換."""
+    tz = timezone("Asia/Tokyo")
+    return parser.parse(utc_str) \
+            .astimezone(tz)
+
+
+def json2server(one: dict) -> Server:
+    """HTTPレスポンスからサーバー情報へ変換.
+
+    :param one: json["servers"]: list[dict]の要素
+    """
+    return Server(
+        ipv4 = one["name"].replace("-", "."),
+        status = Status[one["status"]],
+        created_at = utc2jst(one["created"]),
+        updated_at = utc2jst(one["updated"]),
+        image_id = one["image"]["id"],
+        flavor_id = one["flavor"]["id"],
+    )
+
+
+def list_servers() -> list[Server]:
+    """契約中のサーバー情報一覧を取得する."""
+    tid = env_tenant_id()
+    url = Endpoints.COMPUTE.url(f"{tid}/servers/detail")
+    res = requests.get(url, headers=token_headers(), timeout=3.0)
+    return [json2server(s) for s in res.json()["servers"]]
