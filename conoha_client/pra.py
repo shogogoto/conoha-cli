0db4cf117a5a6ea56ee882e3a55a4d99be697b9b
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-27 00:48:33 +0900

    サーバー一覧表示コマンド、適当に作成

--- conoha_client/pra.py
+++ conoha_client/pra.py
@@ -1,14 +1,50 @@
 """おためしCLI."""
-import click
 
+import click
+from click_shell import shell
+from flatten_dict import flatten
+from tabulate import tabulate
+
+from .api import server as s_api
+
+
+# @click.group()
+@shell(prompt="(conoha-client) ")
+def cli() -> None:
+    """root."""
 
 @click.command()
 @click.option("--greet", help="word to greet", default="hello")
 @click.argument("to")
-def cli(greet:str, to:str) -> None:
+def greet(greet:str, to:str) -> None:
     """お試しCLI."""
     click.echo(f"{greet} {to}")
 
+
+@click.group()
+def server() -> None:
+    """server関連."""
+
+@server.command(name="list")
+def _list() -> None:
+    """契約中サーバー一覧取得コマンド."""
+    import json
+
+    res = [flatten(json.loads(s.json()), reducer="dot") for s in s_api.list_servers()]
+    for s in s_api.list_servers():
+        print(s.model_dump_json(indent=2)) # noqa: T201
+    print(tabulate(res, headers="keys", showindex=True)) #noqa: T201
+
+
+
+@click.group()
+def billing() -> None:
+    """請求・課金関連."""
+
+
 def main() -> None:
     """CLI設定用."""
+    cli.add_command(greet)
+    cli.add_command(billing)
+    cli.add_command(server)
     cli()
