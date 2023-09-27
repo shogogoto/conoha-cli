c4c548785490f364fb670e015ecd95d7a6528351
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-27 20:29:21 +0900

    testファイルもlintする

--- conoha_client/features/share/test_env.py
+++ conoha_client/features/share/test_env.py
@@ -1,34 +1,43 @@
+"""Test getting env vars."""
+
 import os
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
-from . import environments as E
+from conoha_client.features.share import environments
 
 
 @pytest.mark.parametrize(
-    "env_attr", [
+    "env_attr",
+    [
         "OS_USERNAME",
         "OS_PASSWORD",
         "OS_TENANT_ID",
-    ])
-def test_invalid_env_credentials(monkeypatch, env_attr):
+    ],
+)
+def test_invalid_env_credentials(
+    monkeypatch: MonkeyPatch,
+    env_attr: str,
+) -> None:
+    """環境変数が不足しているときにエラー."""
     if env_attr in os.environ:
         monkeypatch.delenv(env_attr)
 
     with pytest.raises(KeyError):
-        E.env_credentials()
+        environments.env_credentials()
 
 
-def test_invalid_env_region(monkeypatch):
+def test_invalid_env_region(monkeypatch: MonkeyPatch) -> None:
+    """OS_CONOHA_REGION_NOにtyo?の数字が入っているか."""
     env_attr = "OS_CONOHA_REGION_NO"
 
     if env_attr in os.environ:
         monkeypatch.delenv(env_attr)
 
     with pytest.raises(KeyError):
-        E.env_region()
+        environments.env_region()
 
     monkeypatch.setenv(env_attr, "NaN")
-    with pytest.raises(ValueError):
-        E.env_region()
-
+    with pytest.raises(ValueError, match=env_attr):
+        environments.env_region()
