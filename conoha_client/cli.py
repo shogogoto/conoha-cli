828b7f712d7bd90ebb416d6be7b6f2952e925d4d
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-27 20:27:24 +0900

    cliファイルリネーム

--- conoha_client/cli.py
+++ conoha_client/cli.py
@@ -5,6 +5,8 @@ from click_shell import shell
 from flatten_dict import flatten
 from tabulate import tabulate
 
+from conoha_client.features.keypars import tmp
+
 from .features.list_servers import list_servers
 
 
@@ -13,10 +15,11 @@ from .features.list_servers import list_servers
 def cli() -> None:
     """root."""
 
+
 @click.command()
 @click.option("--greet", help="word to greet", default="hello")
 @click.argument("to")
-def greet(greet:str, to:str) -> None:
+def greet(greet: str, to: str) -> None:
     """お試しCLI."""
     click.echo(f"{greet} {to}")
 
@@ -25,6 +28,7 @@ def greet(greet:str, to:str) -> None:
 def server() -> None:
     """server関連."""
 
+
 @server.command(name="list")
 def _list() -> None:
     """契約中サーバー一覧取得コマンド."""
@@ -32,9 +36,8 @@ def _list() -> None:
 
     res = [flatten(json.loads(s.json()), reducer="dot") for s in list_servers()]
     for s in list_servers():
-        print(s.model_dump_json(indent=2)) # noqa: T201
-    print(tabulate(res, headers="keys", showindex=True)) #noqa: T201
-
+        print(s.model_dump_json(indent=2))  # noqa: T201
+    print(tabulate(res, headers="keys", showindex=True))  # noqa: T201
 
 
 @click.group()
@@ -47,4 +50,5 @@ def main() -> None:
     cli.add_command(greet)
     cli.add_command(billing)
     cli.add_command(server)
+    cli.add_command(tmp)
     cli()
