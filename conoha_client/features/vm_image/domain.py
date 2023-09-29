6f9953b55762c32af904a3859f7e57df8255d6f0
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-29 17:24:41 +0900

    nested aliasが設定できるみたい

--- conoha_client/features/vm_image/domain.py
+++ conoha_client/features/vm_image/domain.py
@@ -3,20 +3,7 @@ from __future__ import annotations
 
 from uuid import UUID
 
-from pydantic import BaseModel, Field
-
-
-class Metadata(BaseModel, frozen=True):
-    """VPSイメージのメタデータ."""
-
-    dist: str | None = Field(
-        default=None,
-        alias="dst",
-        description="ディストリビューション",
-    )
-    app: str | None = Field(default=None, alias="app", description="アプリケーション")
-    # os: str | None = Field(..., alias="os_type", description="")
-    # 全てlinだったので省く
+from pydantic import AliasPath, BaseModel, Field
 
 
 class Image(BaseModel, frozen=True):
@@ -24,4 +11,6 @@ class Image(BaseModel, frozen=True):
 
     image_id: UUID = Field(alias="id", description="イメージID")
     name: str | None = Field(alias="name", description="名前")
-    metadata: Metadata = Field(alias="metadata", description="メタデータ")
+    dist: str | None = Field(None, alias=AliasPath("metadata", "dst"))
+    app: str | None = Field(None, alias=AliasPath("metadata", "app"))
+    os: str | None = Field(None, alias=AliasPath("metadata", "os_type"))
