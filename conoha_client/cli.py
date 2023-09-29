ea3e188d1dab94ead8af38727dd2e3a972c44532
Shogo Goto <gotoadmn0605@gmail.com>
2023-09-30 00:25:09 +0900

    import改善

--- conoha_client/cli.py
+++ conoha_client/cli.py
@@ -3,10 +3,14 @@
 import click
 from click_shell import shell
 
-from conoha_client.features import list_vm_cli, sshkey_cli
-from conoha_client.features.billing.cli import billing_cli
-from conoha_client.features.image.cli import vm_image_cli
-from conoha_client.features.plan.cli import vm_plan_cli
+from conoha_client.features import (
+    add_vm_cli,
+    billing_cli,
+    list_vm_cli,
+    sshkey_cli,
+    vm_image_cli,
+    vm_plan_cli,
+)
 
 
 # @click.group()
@@ -25,6 +29,7 @@ def main() -> None:
     vm_cli.add_command(vm_plan_cli)
     vm_cli.add_command(vm_image_cli)
     vm_cli.add_command(list_vm_cli)
+    vm_cli.add_command(add_vm_cli)
     cli.add_command(vm_cli)
     cli.add_command(sshkey_cli)
     cli.add_command(billing_cli)
--- conoha_client/features/__init__.py
+++ conoha_client/features/__init__.py
@@ -1,17 +1,8 @@
 """独立した機能のあつまり."""
 
-from conoha_client.features.add_vm import add_vm_cli
-from conoha_client.features.billing import billing_cli
-from conoha_client.features.image import vm_image_cli
-from conoha_client.features.list_vm import list_vm_cli
-from conoha_client.features.plan import vm_plan_cli
-from conoha_client.features.sshkey import sshkey_cli
-
-__all__ = [
-    "sshkey_cli",
-    "list_vm_cli",
-    "add_vm_cli",
-    "vm_plan_cli",
-    "vm_image_cli",
-    "billing_cli",
-]
+from conoha_client.features.add_vm import *  # noqa: F403
+from conoha_client.features.billing import *  # noqa: F403
+from conoha_client.features.image import *  # noqa: F403
+from conoha_client.features.list_vm import *  # noqa: F403
+from conoha_client.features.plan import *  # noqa: F403
+from conoha_client.features.sshkey import *  # noqa: F403
