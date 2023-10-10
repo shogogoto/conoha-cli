4d9035fe4b4d27cf700c5f1fee6f8e57d1edc7c6
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-10 11:52:41 +0900

    add VM shutdown CLI

--- conoha_client/features/vm_actions/cli.py
+++ conoha_client/features/vm_actions/cli.py
@@ -6,7 +6,7 @@ from uuid import UUID
 
 import click
 
-from conoha_client.features.vm_actions.repo import remove_vm
+from conoha_client.features.vm_actions.repo import VMActionCommands, remove_vm
 
 
 @click.group()
@@ -21,14 +21,13 @@ def vm_actions_cli() -> None:
     "-f",
     type=click.File("r"),
     default="-",
-    help="削除対象のUUIDをファイルから読み取る(default:標準入力)",
+    help="対象のUUIDをファイル入力(default:標準入力)",
 )
 def remove(
     vm_ids: tuple[UUID],
     file: TextIO,
 ) -> None:
     """VM削除."""
-    click.echo("remove vm")
     vm_ids = list(vm_ids)
     if not file.isatty():
         lines = file.read().splitlines()
@@ -36,3 +35,28 @@ def remove(
         vm_ids.extend(uids)
     for vm_id in vm_ids:
         remove_vm(vm_id)
+        click.echo(f"{vm_id} was removed.")
+
+
+@vm_actions_cli.command()
+@click.argument("vm_ids", nargs=-1, type=click.UUID)
+@click.option(
+    "--file",
+    "-f",
+    type=click.File("r"),
+    default="-",
+    help="対象のUUIDをファイル入力(default:標準入力)",
+)
+def shutdown(
+    vm_ids: tuple[UUID],
+    file: TextIO,
+) -> None:
+    """VMシャットダウン."""
+    vm_ids = list(vm_ids)
+    if not file.isatty():
+        lines = file.read().splitlines()
+        uids = [UUID(line) for line in lines]
+        vm_ids.extend(uids)
+    for vm_id in vm_ids:
+        cmd = VMActionCommands(vm_id=vm_id)
+        cmd.shutdown()
