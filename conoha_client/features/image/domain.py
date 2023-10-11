58d13c2f7e4508b2ddecd072ed106b502442a1cd
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-11 17:47:26 +0900

    add properties of Image model

--- conoha_client/features/image/domain.py
+++ conoha_client/features/image/domain.py
@@ -1,9 +1,12 @@
 """VM Image Domain."""
 from __future__ import annotations
 
+from datetime import datetime
 from uuid import UUID
 
-from pydantic import AliasPath, BaseModel, Field
+from pydantic import AliasPath, BaseModel, Field, field_validator
+
+from conoha_client.features._shared.util import utc2jst
 
 
 class Image(BaseModel, frozen=True):
@@ -14,3 +17,13 @@ class Image(BaseModel, frozen=True):
     dist: str = Field("", alias=AliasPath("metadata", "dst"))
     app: str = Field("", alias=AliasPath("metadata", "app"))
     os: str = Field(alias=AliasPath("metadata", "os_type"))
+    created: str = Field(alias="created", description="作成日時")
+    minDisk: int = Field(  # noqa: N815
+        alias="minDisk",
+        description="インスタンス化に必要なディスク容量",
+    )
+
+    @field_validator("created")
+    def validate_created(cls, v: str) -> datetime:  # noqa: N805
+        """Validate created datetime."""
+        return utc2jst(v)
