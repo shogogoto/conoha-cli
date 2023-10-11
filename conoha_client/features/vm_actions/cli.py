eb3f88984dd27990fbbe8e4b8f48c48a253278d9
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-11 16:32:02 +0900

    VM uuid cli option for common input CLI commands.

--- conoha_client/features/vm_actions/cli.py
+++ conoha_client/features/vm_actions/cli.py
@@ -1,12 +1,13 @@
 """VM操作CLI."""
 from __future__ import annotations
 
-from typing import TextIO
 from uuid import UUID
 
 import click
 
-from conoha_client.features.vm_actions.repo import VMActionCommands, remove_vm
+from conoha_client.features.vm_actions.command_option import uuid_target_options
+
+from .repo import VMActionCommands, remove_vm
 
 
 @click.group()
@@ -15,48 +16,16 @@ def vm_actions_cli() -> None:
 
 
 @vm_actions_cli.command(name="rm")
-@click.argument("vm_ids", nargs=-1, type=click.UUID)
-@click.option(
-    "--file",
-    "-f",
-    type=click.File("r"),
-    default="-",
-    help="対象のUUIDをファイル入力(default:標準入力)",
-)
-def remove(
-    vm_ids: tuple[UUID],
-    file: TextIO,
-) -> None:
+@uuid_target_options
+def remove_cli(vm_id: UUID) -> None:
     """VM削除."""
-    vm_ids = list(vm_ids)
-    if not file.isatty():
-        lines = file.read().splitlines()
-        uids = [UUID(line) for line in lines]
-        vm_ids.extend(uids)
-    for vm_id in vm_ids:
-        remove_vm(vm_id)
-        click.echo(f"{vm_id} was removed.")
+    remove_vm(vm_id)
+    click.echo(f"{vm_id} was removed.")
 
 
-@vm_actions_cli.command()
-@click.argument("vm_ids", nargs=-1, type=click.UUID)
-@click.option(
-    "--file",
-    "-f",
-    type=click.File("r"),
-    default="-",
-    help="対象のUUIDをファイル入力(default:標準入力)",
-)
-def shutdown(
-    vm_ids: tuple[UUID],
-    file: TextIO,
-) -> None:
+@vm_actions_cli.command(name="shutdown")
+@uuid_target_options
+def shutdown_cli(vm_id: UUID) -> None:
     """VMシャットダウン."""
-    vm_ids = list(vm_ids)
-    if not file.isatty():
-        lines = file.read().splitlines()
-        uids = [UUID(line) for line in lines]
-        vm_ids.extend(uids)
-    for vm_id in vm_ids:
-        cmd = VMActionCommands(vm_id=vm_id)
-        cmd.shutdown()
+    cmd = VMActionCommands(vm_id=vm_id)
+    cmd.shutdown()
