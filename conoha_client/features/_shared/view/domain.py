8e5d64ba3f3859ee03195ec659a50b1e690aad8f
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-13 18:42:24 +0900

    add where option of view

--- conoha_client/features/_shared/view/domain.py
+++ conoha_client/features/_shared/view/domain.py
@@ -31,7 +31,7 @@ def check_include_keys(model: R, keys: set[str]) -> None:
         raise ExtraKeyError(msg)
 
 
-def model_filter(model: R, keys: set[str] | None = None) -> dict:
+def model_extract(model: R, keys: set[str] | None = None) -> dict:
     """モデルから特定のプロパティのみのjsonへ変換.
 
     :param model: (BaseModel)ドメインモデル
@@ -40,6 +40,16 @@ def model_filter(model: R, keys: set[str] | None = None) -> dict:
     """
     check_include_keys(model, keys)
     return model.model_dump(mode="json", include=keys)
+
+
+def model_filter(models: list[R], key: str, value: str) -> list[R]:
+    """modelをフィルターする."""
+
+    def pred(e: R) -> bool:
+        check_include_keys(e, {key})
+        return value in str(getattr(e, key))
+
+    return list(filter(pred, models))
 
 
 def _tabulate(js: list[dict], pass_command: bool) -> str:
@@ -60,7 +70,7 @@ def view(
     pass_command: bool,
 ) -> None:
     _keys = set(keys) if len(keys) > 0 else None
-    js = [model_filter(m, _keys) for m in models]
+    js = [model_extract(m, _keys) for m in models]
 
     if style == "json":
         txt = json.dumps(js, indent=2)
@@ -83,6 +93,13 @@ def view_options(func: Callable[P, list[R]]) -> Callable[P, None]:
 
     @click.option("--keys", "-k", multiple=True, default=None)
     @click.option(
+        "--where",
+        "-w",
+        nargs=2,
+        type=click.Tuple([str, str]),
+        help="マッチした行をkey-valueでフィルターする",
+    )
+    @click.option(
         "--style",
         "-s",
         type=click.Choice(["table", "json"]),
@@ -99,12 +116,15 @@ def view_options(func: Callable[P, list[R]]) -> Callable[P, None]:
     @functools.wraps(func)
     def wrapper(
         keys: set[str],
+        where: tuple[str, str],
         style: Style,
         pass_command: bool,
         *args: P.args,
         **kwargs: P.kwargs,
     ) -> None:
         models = func(*args, **kwargs)
+        if where is not None:
+            models = model_filter(models, key=where[0], value=where[1])
         view(models, keys, style, pass_command)
 
     return wrapper
