711eee04a82e61504852b639d0b6b124f3e92d08
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-28 14:24:37 +0900

    fix return linux images

--- conoha_client/features/image/repo.py
+++ conoha_client/features/image/repo.py
@@ -4,16 +4,13 @@ from __future__ import annotations
 from functools import cache
 
 from conoha_client.features import Endpoints
+from conoha_client.features.image.domain.image import ImageList
 
 from .domain import Image
 
 
 @cache
-def list_images() -> list[Image]:
+def list_images() -> ImageList:
     """イメージ一覧を取得する."""
     res = Endpoints.COMPUTE.get("images/detail").json()
-    return [Image.model_validate(e) for e in res["images"]]
-
-
-# dist指定でバージョン一覧を取得
-# human friendlyな指定でimage id を探せるようにしたい
+    return ImageList([Image.model_validate(e) for e in res["images"]])
