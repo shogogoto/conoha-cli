2fd11db31f31ee6d39777ed1bd585b8c413a1f69
Shogo Goto <gotoadmn0605@gmail.com>
2023-10-28 12:52:05 +0900

    fix import

--- conoha_client/add_vm/cli.py
+++ conoha_client/add_vm/cli.py
@@ -2,18 +2,24 @@
 from __future__ import annotations
 
 import os
+from operator import attrgetter
 from typing import TYPE_CHECKING
 
 import click
 
+from conoha_client.add_vm.repo import DistQuery
 from conoha_client.features._shared.view.domain import view_options
-from conoha_client.features.image.domain.operating_system import (
+from conoha_client.features.image.domain import (
+    Application,
     Distribution,
+    DistVersion,
 )
 from conoha_client.features.plan.domain import Memory
 
 if TYPE_CHECKING:
-    from conoha_client.features.list_vm.domain import Server
+    from conoha_client.features.vm.domain import VM
+
+    pass
 
 
 @click.group("add", invoke_without_command=True)
@@ -24,31 +30,33 @@ if TYPE_CHECKING:
     required=True,
 )
 @click.option(
-    "--disto",
+    "--dist",
     "-d",
     type=click.Choice(Distribution),
     default=Distribution.UBUNTU,
     show_default=True,
 )
 @click.option(
-    "--os-version",
-    "-ov",
+    "--version",
+    "-v",
     default="latest",
     help="latestの場合最新バージョンが指定される",
     show_default=True,
+    type=DistVersion.parse,
 )
 @click.option(
     "--app",
     "-a",
-    default="NONE",
+    default="null",
     help="アプリ名.NONEは指定なし",
     show_default=True,
+    type=Application.parse,
 )
 @click.option(
-    "--app-version",
-    "-av",
-    default="NONE",
-    help="アプリのバージョン.NONEは指定なし",
+    "--admin-password",
+    "-pw",
+    default=lambda: os.getenv("OS_ADMIN_PASSWORD", None),
+    help="VMのrootユーザーのパスワード:OS_ADMIN_PASSWORD環境変数の値が設定される",
     show_default=True,
 )
 @click.option(
@@ -58,66 +66,54 @@ if TYPE_CHECKING:
     help="sshkeyのペア名:OS_SSHKEY_NAME環境変数の値が設定される",
     show_default=True,
 )
-@click.option(
-    "--admin-password",
-    "-pw",
-    default=lambda: os.getenv("OS_ADMIN_PASSWORD", None),
-    help="VMのrootユーザーのパスワード:OS_ADMIN_PASSWORD環境変数の値が設定される",
-    show_default=True,
-)
 @view_options
 @click.pass_context
-def add_vm_cli(
+def add_vm_cli(  # noqa: PLR0913
     ctx: click.Context,
-    # memory: Memory,
-    # disto: Distribution,
-    # os_version: str,
-    # app: str,
-    # app_version: str,
-    # admin_password: str | None,
-    # keypair_name: str | None,
-) -> list[Server]:
+    memory: Memory,
+    dist: Distribution,
+    version: str,
+    app: str,
+    admin_password: str | None,
+    keypair_name: str | None,
+) -> list[VM]:
     """Add VM CLI."""
     ctx.ensure_object(dict)
-    # repo = ImageInfoRepo(memory=memory, os=os)
-    # osv = OSVersion(value=os_version, os=os)
-    # appv = Application(name=app, version=app_version)
-    # ctx.obj["repo"] = repo
-    # ctx.obj["os_version"] = osv
-    # ctx.obj["app"] = appv
-    # if ctx.invoked_subcommand is None:
-    #     added = add_vm(
-    #         repo,
-    #         # osv,
-    #         appv,
-    #         admin_password,
-    #         keypair_name,
-    #     )
+    query = DistQuery(memory=memory, dist=dist)
+    # a = Application.parse(app)
+    ctx.obj["q"] = query
+    ctx.obj["version"] = version
+    ctx.obj["app"] = app
+    if ctx.invoked_subcommand is None:
+        click.echo(admin_password)
+        click.echo(keypair_name)
+        # cmd = add_vm_command(
+        #     memory=memory,
+        #     dist=dist,
+        #     ver=version,
+        #     app=app,
+        #     admin_pass=admin_password,
+        # )
+        # click.echo(cmd)
     #     click.echo(":以下のVMが新規追加されました")
     #     return [added]
     return []
 
 
-# @add_vm_cli.command(name="os-vers")
-# @view_options
-# @click.pass_obj
-# def list_os_versions(obj: object) -> list[OSVersion]:
-#     """利用可能なOSバージョンを検索する."""
-#     return obj["repo"].available_os_versions
+@add_vm_cli.command(name="dist-vers")
+@view_options
+@click.pass_obj
+def list_os_versions(obj: object) -> list[DistVersion]:
+    """利用可能なOSバージョンを検索する."""
+    vers = obj["q"].available_vers()
+    return sorted(vers, key=attrgetter("value"))
 
 
-# @add_vm_cli.command(name="os-latest")
-# @view_options
-# @click.pass_obj
-# def find_latest_os_version(obj: object) -> list[OSVersion]:
-#     """利用可能な最新のOSバージョンを検索する."""
-#     v = obj["repo"].available_os_latest_version
-#     return [v]
-
-
-# @add_vm_cli.command(name="apps")
-# @view_options
-# @click.pass_obj
-# def find_apps(obj: object) -> list[Application]:
-#     """利用可能なアプリケーションを検索する."""
-#     return obj["repo"].list_available_apps(obj["os_version"])
+@add_vm_cli.command(name="apps")
+@view_options
+@click.pass_obj
+def find_apps(obj: object) -> list[Application]:
+    """利用可能なアプリケーションを検索する."""
+    v = obj["version"]
+    apps = obj["q"].apps(v)
+    return sorted(apps, key=attrgetter("value"))
--- conoha_client/add_vm/usecase.py
+++ conoha_client/add_vm/usecase.py
@@ -1,20 +1,7 @@
 """Usecase."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 import click
-
-from conoha_client.add_vm.repo import (
-    AddVMCommand,
-    ImageInfoRepo,
-    find_added,
-)
-from conoha_client.features.plan.repo import find_vmplan
-
-if TYPE_CHECKING:
-    from conoha_client.add_vm.domain.domain import Application, OSVersion
-    from conoha_client.features.list_vm.domain import Server
 
 
 def get_password(value: str | None) -> str:
@@ -31,23 +18,3 @@ def get_sshkey_name(value: str | None) -> str | None:
         return value
     msg = "VMに紐付けるsshkey名を入力してくだいさい"
     return click.prompt(msg, hide_input=True, confirmation_prompt=True)
-
-
-def add_vm(
-    repo: ImageInfoRepo,
-    os_version: OSVersion,
-    app: Application,
-    admin_pass: str | None,
-    sshkey_name: str | None = None,
-) -> Server:
-    """Add VM Usecase."""
-    flavor = find_vmplan(repo.memory)
-    image_id = repo.find_image_id(os_version, app)
-
-    cmd = AddVMCommand(
-        flavor_id=flavor.flavor_id,
-        image_id=image_id,
-        admin_pass=get_password(admin_pass),
-    )
-    added = cmd(get_sshkey_name(sshkey_name))
-    return find_added(added.vm_id)
