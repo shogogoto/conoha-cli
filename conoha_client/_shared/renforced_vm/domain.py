19fea1f50f5424a5f4ab4b8cfa2ee304dd1afbf0
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-06 12:02:05 +0900

    reinforced VM query

---
+++ conoha_client/_shared/renforced_vm/domain.py
@@ -0,0 +1,25 @@
+"""reinforced VM domain."""
+from __future__ import annotations
+
+from datetime import timedelta
+from ipaddress import IPv4Address
+
+from pydantic import BaseModel, Field, field_serializer
+
+from conoha_client.features.vm.domain import VMStatus  # noqa: TCH001
+
+
+class ReinforcedVM(BaseModel, frozen=True):
+    """human friendly VM, not just API Return."""
+
+    ipv4: IPv4Address
+    status: VMStatus
+    elapsed: timedelta = Field()
+    memoryMB: int = Field(alias="mem_mb")  # noqa: N815
+    n_cpu: int = Field(alias="n_core")
+    storageGB: int = Field(alias="disk_gb")  # noqa: N815
+    sshkey: str | None
+
+    @field_serializer("elapsed")
+    def _serialize(self, v: timedelta) -> str:
+        return str(v)
