3926ea3a0d4d171cfed838b4a2cdfed9c12f0be6
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-09 22:44:56 +0900

    remove prepare for test because not to require requests_mock for pip install

--- conoha_client/features/_shared/__init__.py
+++ conoha_client/features/_shared/__init__.py
@@ -1,7 +1,6 @@
 """featuresから利用される共有ライブラリ."""
 
 from .command_option import *  # noqa: F403
-from .conftest import prepare  # noqa: F401
 from .endpoints import *  # noqa: F403
 from .prompt import *  # noqa: F403
 from .util import *  # noqa: F403
