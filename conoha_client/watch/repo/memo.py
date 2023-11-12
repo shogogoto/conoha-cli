11443bc87367e57e5932eadf75a93691f9054f16
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-12 21:05:16 +0900

    vm elapsed time memo

--- conoha_client/watch/repo/memo.py
+++ conoha_client/watch/repo/memo.py
@@ -1,6 +1,7 @@
 """query memorize."""
 
 
+from datetime import timedelta
 from typing import Callable
 from uuid import UUID
 
@@ -13,28 +14,38 @@ from conoha_client.features.vm.repo.query import complete_vm, list_vms
 def vm_status_finder(vm_id: UUID) -> Callable[[], VMStatus]:
     """Find status by id. memo."""
 
-    def _func() -> VMStatus:
+    def _f() -> VMStatus:
         vm = complete_vm(str(vm_id))
         return vm.status
 
-    return _func
+    return _f
 
 
 def snapshot_progress_finder(name: str) -> Callable[[], int]:
     """Find progress by name memo."""
 
-    def _func() -> int:
+    def _f() -> int:
         image = complete_snapshot_by_name(name)
         return image.progress
 
-    return _func
+    return _f
 
 
 def exists_vm(vm_id: UUID) -> Callable[[], bool]:
     """Exists vm memo."""
 
-    def _func() -> bool:
+    def _f() -> bool:
         vm = ModelList(list_vms()).find_one_or_none_by(by("vm_id", vm_id))
         return vm is not None
 
-    return _func
+    return _f
+
+
+def elapsed_from_created(vm_id: UUID) -> Callable[[], timedelta]:
+    """Elapsed time memo."""
+
+    def _f() -> timedelta:
+        vm = complete_vm(str(vm_id))
+        return vm.elapsed_from_created()
+
+    return _f
