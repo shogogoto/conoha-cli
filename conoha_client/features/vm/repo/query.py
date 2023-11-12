8eab3c55d052ff967f5fabf51a90b1bfbefb8fa8
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-12 12:54:54 +0900

    remove unused query

--- conoha_client/features/vm/repo/query.py
+++ conoha_client/features/vm/repo/query.py
@@ -7,7 +7,6 @@ from uuid import UUID
 from conoha_client.features._shared import Endpoints
 from conoha_client.features._shared.model_list.domain import ModelList, startswith
 from conoha_client.features.vm.domain import VM
-from conoha_client.features.vm.errors import NotFoundAddedVMError
 
 
 def get_dep() -> list[object]:
@@ -32,21 +31,3 @@ def complete_vm(s: str) -> VM:
 def complete_vm_id(s: str) -> UUID:
     """uuidを補完して検索."""
     return complete_vm(s).vm_id
-
-
-def find_added(
-    vm_id: UUID,
-    dep: Callable[[], list[object]] = get_dep,
-) -> VM:
-    """Return ipv4 of added vm."""
-
-    def pred(vm: VM) -> bool:
-        return vm.vm_id == vm_id
-
-    f = filter(pred, list_vms(dep=dep))
-
-    try:
-        return next(f)
-    except StopIteration as e:
-        msg = ""
-        raise NotFoundAddedVMError(msg) from e
