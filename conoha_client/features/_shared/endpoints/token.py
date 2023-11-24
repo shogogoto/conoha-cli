3f50b72cae17fef2d77eb0632b894376526e12c9
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-24 13:46:19 +0900

    remove token cache for avoiding expired

--- conoha_client/features/_shared/endpoints/token.py
+++ conoha_client/features/_shared/endpoints/token.py
@@ -1,20 +1,18 @@
 """認証周りの処理."""
 from __future__ import annotations
 
-from functools import cache
-
 import requests
 
 from . import endpoints
 from .environments import env_credentials
 
 
-@cache
 def issue_token_id() -> str:
     """ConoHa API用のトークンを発行する."""
     url = endpoints.Endpoints.IDENTITY.url("tokens")
     res = requests.post(url, json=env_credentials(), timeout=3.0)
     return res.json()["access"]["token"]["id"]
+
 
 def token_headers() -> dict[str, str]:
     """ConoHa API用のヘッダーを作成する."""
@@ -22,5 +20,3 @@ def token_headers() -> dict[str, str]:
         "Accept": "application/json",
         "X-Auth-Token": issue_token_id(),
     }
-
-
