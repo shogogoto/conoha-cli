6aeb13a4a8fedcd9c0528f0e7ce13651197b3901
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-28 22:30:02 +0900

    build_vm_options_factory

--- conoha_client/features/_shared/command_option/build_vm_options.py
+++ conoha_client/features/_shared/command_option/build_vm_options.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import functools
-import os
 from typing import Callable, Concatenate, ParamSpec, TypeVar
 
 import click
@@ -11,38 +10,48 @@ from conoha_client.features._shared.prompt import pw_prompt, sshkey_prompt
 P = ParamSpec("P")
 T = TypeVar("T")
 
+Wrapped = Callable[Concatenate[str, str, P], T]
 
-def build_vm_options(
-    func: Callable[Concatenate[str, str, P], T],
-) -> Callable[Concatenate[str, str, P], T]:
-    """Add VM共通オプション."""
 
-    @click.option(
-        "--admin-password",
-        "-pw",
-        default=lambda: os.getenv("OS_ADMIN_PASSWORD", None),
-        help="VMのrootユーザーのパスワード:OS_ADMIN_PASSWORD環境変数の値が設定される",
-        show_default=True,
-    )
-    @click.option(
-        "--keypair-name",
-        "-k",
-        default=lambda: os.getenv("OS_SSHKEY_NAME", None),
-        help="sshkeyのペア名:OS_SSHKEY_NAME環境変数の値が設定される",
-        show_default=True,
-    )
-    @functools.wraps(func)
-    def wrapper(
-        admin_password: str | None,
-        keypair_name: str | None,
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ) -> T:
-        return func(
-            *args,
-            admin_password=admin_password or pw_prompt(),
-            keypair_name=keypair_name or sshkey_prompt(),
-            **kwargs,
+def build_vm_options_factory(pw_envvar: str, kw_envvar: str) -> Callable:
+    def f(func: Wrapped) -> Wrapped:
+        """Add VM共通オプション."""
+
+        @click.option(
+            "--admin-password",
+            "-pw",
+            envvar=pw_envvar,
+            show_envvar=True,
+            help="VMのrootユーザーのパスワード",
         )
+        @click.option(
+            "--keypair-name",
+            "-k",
+            envvar=kw_envvar,
+            show_envvar=True,
+            help="sshkeyのペア名",
+            show_default=True,
+        )
+        @functools.wraps(func)
+        def wrapper(
+            admin_password: str | None,
+            keypair_name: str | None,
+            *args: P.args,
+            **kwargs: P.kwargs,
+        ) -> T:
+            return func(
+                *args,
+                admin_password=admin_password or pw_prompt(),
+                keypair_name=keypair_name or sshkey_prompt(),
+                **kwargs,
+            )
 
-    return wrapper
+        return wrapper
+
+    return f
+
+
+build_vm_options = build_vm_options_factory(
+    "OS_ADMIN_PASSWORD",
+    "OS_SSHKEY_NAME",
+)
